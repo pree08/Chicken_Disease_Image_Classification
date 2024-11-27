58135cc96a847ac080b6a65eec4b23b0f51c734b
pree08 <preetam.pree07@gmail.com>
2024-11-27 16:38:46 +0530

    prepare base model ase model completed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from src.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from src.logger import logger
 
 STAGE_NAME = "Data Ingestion stage"
@@ -7,6 +8,18 @@ try:
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
     logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx===========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Prepare base model"
+try:
+    logger.info(f"*****************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    prepare_base_model = PrepareBaseModelTrainingPipeline()
+    prepare_base_model.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx============x")
 except Exception as e:
     logger.exception(e)
     raise e
